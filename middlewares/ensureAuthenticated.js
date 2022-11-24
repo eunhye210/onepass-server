02a64364fd6a064bdcfccc1781c5bfb317e3d455
eunhye210 <tkrhd0210@gmail.com>
2022-11-24 22:15:31 +0900

    fix: password 에러 확인

--- middlewares/ensureAuthenticated.js
+++ middlewares/ensureAuthenticated.js
@@ -9,13 +9,13 @@ async function ensureAuthenticated(req, res, next) {
   const { sessionKey } = req.cookies;
 
   if (!mongoose.Types.ObjectId.isValid(userId)) {
-    return res.status(404).json({ errorMessage: ERROR.SERVER_ERROR });
+    return res.status(404).json({ errorMessage: "error1" }); // ERROR.SERVER_ERROR
   }
 
   const user = await User.findById(userId);
 
   if (!sessionKey || !user?.sessionKey.includes(sessionKey)) {
-    return res.status(404).json({ errorMessage: ERROR.AUTH_ERROR });
+    return res.status(404).json({ errorMessage: "error2" }); // ERROR.AUTH_ERROR
   }
 
   next();
